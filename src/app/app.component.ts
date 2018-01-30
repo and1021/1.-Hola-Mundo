62347050c66515e786c8484e2186dc07ad789a6a
Andrey Fernando Ortiz Rodriguez <andreymafesara@hotmail.com>
2018-01-30 16:22:22 -0500

    Prueba commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  nombre:string = 'Fernando';
+  nombre:string = 'Fernando2';
   apellido:string = 'Rodriguez';
 }
