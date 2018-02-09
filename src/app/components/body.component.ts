7575ab1ca943759a3eb9d4977c0c3228bcc99fdc
Andrey Fernando Ortiz Rodriguez <andreymafesara@hotmail.com>
2018-02-09 07:46:04 -0500

    Avance de curso clase 40

--- src/app/components/body.component.ts
+++ src/app/components/body.component.ts
@@ -2,7 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-body',
-  templateUrl:'body.component.html' 
+  templateUrl: 'body.component.html'
 })
 export class BodyComponent {
+  frase: any = {
+    titulo: 'Frase del dia',
+    mensaje: 'Un gran poder requiere una gran responsablidad',
+    autor: 'Ben Parker'
+  };
+  mostrar = false;
+  personajes: string[] = ['Spiderman', 'Venom', 'Dr.Octopus'];
+  villanos: any[] = [
+    {
+      nombre: 'Spiderman',
+      descripcion: 'Hero de New York'
+    },
+    {
+      nombre: 'Venom',
+      descripcion: 'Nemesis de Spiderman'
+    },
+    {
+      nombre: 'Dr.Octopus',
+      descripcion: 'Enemigo de Spiderman'
+    }];
 }
